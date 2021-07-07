4814d8ab766ba12a946a95000f85f3b7e0c94f19
Denis Singh <denis.darkrgd@gmail.com>
2021-07-07 20:03:02 +0300

    Added task-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Profile from './components/Profile/Profile';
 import user from './data/user.json';
 
+import Statistics from './components/Statistics/Statistics';
+import statisticsData from './data/statistical-data.json';
+
 const App = () => {
   return (
-    // <>
-    <Profile
-      name={user.name}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-    // </>
+    <>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload Stats" stats={statisticsData} />
+    </>
   );
 };
 
