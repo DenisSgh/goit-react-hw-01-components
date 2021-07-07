5b3d996ba13e725d7aea3dd5851c2bc39b50f026
Denis Singh <denis.darkrgd@gmail.com>
2021-07-07 19:28:04 +0300

    Added task-1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-function App() {
+import Profile from './components/Profile/Profile';
+import user from './data/user.json';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    // </>
   );
-}
+};
 
 export default App;
