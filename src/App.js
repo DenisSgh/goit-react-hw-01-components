5dbd8232650033c7c6075ca15b3bb70440245e68
Denis Singh <denis.darkrgd@gmail.com>
2021-07-08 14:13:32 +0300

    Added task-4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import user from './data/user.json';
 import Statistics from './components/Statistics/Statistics';
 import statisticsData from './data/statistical-data.json';
 
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './data/transactions.json';
+
 const App = () => {
   return (
     <>
@@ -15,6 +18,7 @@ const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload Stats" stats={statisticsData} />
+      <TransactionHistory transactions={transactions} />
     </>
   );
 };
