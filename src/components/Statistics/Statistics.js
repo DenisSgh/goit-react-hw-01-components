344283711eff00c1e27a4d949b80fcae90020e69
Denis Singh <denis.darkrgd@gmail.com>
2021-07-08 20:00:55 +0300

    Added task-2 styles

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,15 +1,26 @@
 import PropTypes from 'prop-types';
+import styles from './Statistics.module.css';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
+    <section className={styles.statistics}>
+      <h2 className={styles.title}>{title}</h2>
 
-      <ul className="stat-list">
+      <ul className={styles.statList}>
         {stats.map(({ id, label, percentage }) => (
-          <li className="item" key={id}>
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}%</span>
+          <li
+            className={styles.item}
+            key={id}
+            style={{
+              backgroundColor:
+                '#' +
+                (Math.random().toString(16) + '000000')
+                  .substring(2, 8)
+                  .toUpperCase(),
+            }}
+          >
+            <span className={styles.label}>{label}</span>
+            <span className={styles.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
@@ -29,3 +40,5 @@ Statistics.propTypes = {
 };
 
 export default Statistics;
+// () =>
+//   '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
